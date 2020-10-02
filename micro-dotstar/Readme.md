3af2ad7f88ec4f47c406ede81892da8c39043511
Craig Holzinger <clholzin@gmail.com>
2020-10-01 18:01:55 -0700

    refactor

--- micro-dotstar/Readme.md
+++ micro-dotstar/Readme.md
@@ -1,3 +1,47 @@
 ## run it
 
 tinygo flash -target=microbit ./main.go
+
+### GPIO breadboard
+
+```
+	SPI0_SCK_PIN  Pin = 23 // P13 on the board
+	SPI0_MOSI_PIN Pin = 21 // P15 on the board
+	SPI0_MISO_PIN Pin = 22 // P14 on the board
+
+  const clock = machine.SPI0_SCK_PIN
+	clock.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	machine.SPI0.Configure(machine.SPIConfig{
+		Frequency: 4000000,
+		SCK:       clock,
+		MISO:      machine.SPI0_MISO_PIN,
+		MOSI:      machine.SPI0_MOSI_PIN,
+		Mode:      0})
+```
+
+### Gator Clips
+
+```
+	P0  Pin = 3
+	P1  Pin = 2
+	P2  Pin = 1
+
+  const clock = machine.P1
+	clock.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	machine.SPI0.Configure(machine.SPIConfig{
+		Frequency: 4000000,
+		SCK:       clock,
+		MISO:      machine.P2,
+		MOSI:      machine.P0,
+		Mode:      0})
+```
+
+### colors
+
+```
+	bufOrange      = []byte{x, 0x00, 0x5a, 0xa8} // 0xd4, 0x51, 0x0b}
+	bufYellow      = []byte{x, 0x08, 0x8c, 0xff} // 0xd4, 0x51, 0x0b}
+	bufBlue        = []byte{x, 0xf5, 0x87, 0x42}
+	bufGreen       = []byte{x, 0x0b, 0xd4, 0x4e}
+	bufMag         = []byte{x, 0xd4, 0x0b, 0xa2}
+```
--- micro-dotstar/main.go
+++ micro-dotstar/main.go
@@ -7,25 +7,29 @@ import (
 	"time"
 )
 
-// cs is the pin used for Chip Select (CS). Change to whatever is in use on your board.
-const clock = machine.SPI0_SCK_PIN
-
 var (
-	tx        []byte
-	rx        []byte
-	numpix    int  = 144
-	x         byte = 0xef // brightness control E0 to FF
-	r         byte = 0xff // red value
-	g         byte = 0x0f // green value
-	b         byte = 0xab // blue value
-	buf            = []byte{x, b, g, r}
-	bufOrange      = []byte{x, 0xd4, 0x51, 0x0b}
-	bufBlue        = []byte{x, 0x42, 0x87, 0xf5}
-	bufGreen       = []byte{x, 0x4e, 0xd4, 0x0b}
+	tx     []byte
+	rx     []byte
+	numpix int = 144
+
+	x   byte = 0xef     // brightness control E0 to FF
+	r   byte = 0xff     // red value
+	g   byte = 0x0f     // green value
+	b   byte = 0xab     // blue value
+	buf      = bufWhite //[]byte{x, b, g, r}
+
+	bufWhite   = []byte{x, 0xff, 0xff, 0xff}
+	bufOrange  = []byte{x, 0x00, 0x5a, 0xa8} // 0xd4, 0x51, 0x0b}
+	bufYellow  = []byte{x, 0xff, 0xea, 0x05} // 0xd4, 0x51, 0x0b}
+	bufOrange2 = []byte{x, 0x00, 0xc4, 0xff} //[]byte{x, 0xf5, 0x87, 0x42}
+	bufGreen   = []byte{x, 0x1e, 0xdb, 0x14}
+	bufGreen2  = []byte{x, 0x1b, 0xde, 0x5f}
+	bufMag     = []byte{x, 0xad, 0x8a, 0xff} // []byte{x, 0xd4, 0x0b, 0xa2}
+
+	start, end int = 0, 10
 )
 
 func main() {
-	clock.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
 	ledMagRow := machine.LED_ROW_1
 	ledMagRow.Configure(machine.PinConfig{Mode: machine.PinOutput})
@@ -33,22 +37,26 @@ func main() {
 	ledMag := machine.LED_COL_1
 	ledMag.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
+	// cs is the pin used for Chip Select (CS). Change to whatever is in use on your board.
+	const clock = machine.P1
+	clock.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	machine.SPI0.Configure(machine.SPIConfig{
 		Frequency: 4000000,
 		SCK:       clock,
-		MISO:      machine.SPI0_MISO_PIN,
-		MOSI:      machine.SPI0_MOSI_PIN,
+		MISO:      machine.P2,
+		MOSI:      machine.P0,
 		Mode:      0})
 
 	ledMagRow.High()
+
 	clock.High()
 	var toggle int
 	for {
 		ledMag.Low()
+		wleds := All(toggle)
 		toggle++
-		wleds := Leds(toggle)
-		if toggle == 3 {
-			toggle = 0
+		if toggle > 2 {
+			toggle = 2
 		}
 		//rleds := make([]byte, len(wleds))
 		clock.Low()
@@ -59,26 +67,65 @@ func main() {
 			break
 		}
 		clock.High()
+
+		/*for i := 0; i < numpix; i++ {
+			copy := wleds[:]
+			en := i + 1
+			if en > numpix {
+				en = numpix
+			}
+			copy = Zip(i, en, copy)
+			clock.Low()
+			err := machine.SPI0.Tx(copy, nil)
+			if err != nil {
+				fmt.Printf("%s\n", err)
+				clock.High()
+				break
+			}
+			clock.High()
+			time.Sleep(50 * time.Millisecond)
+
+		}*/
+
 		//fmt.Printf("%v\n", rleds)
 		ledMag.High()
-		time.Sleep(300 * time.Millisecond)
+		time.Sleep(1000 * time.Millisecond)
 
 	}
 }
 
-func Leds(toggle int) (tx []byte) {
-	tx = append([]byte{}, []byte{0x00, 0x00, 0x00, 0x00}...)
+func Zip(start, end int, tx []byte) (tz []byte) {
+	startbyte := tx[:start*4]
+	endbyte := tx[end*4:]
+	runbyte := []byte{}
+	for i := 0; i < (start - end); i++ {
+		runbyte = append(runbyte, buf...)
+	}
+	tz = append(startbyte, runbyte...)
+	tz = append(tz, endbyte...)
+	return tz
+}
+
+func All(toggle int) (tx []byte) {
+	tx = append(tx, []byte{0x00, 0x00, 0x00, 0x00}...)
 	for i := 0; i < numpix; i++ {
 		switch toggle {
 		case 1:
 			tx = append(tx, bufOrange...)
 			break
 		case 2:
-			tx = append(tx, bufBlue...)
+			tx = append(tx, bufYellow...)
 			break
 		case 3:
 			tx = append(tx, bufGreen...)
 			break
+		case 4:
+			tx = append(tx, bufGreen2...)
+			break
+		case 5:
+			tx = append(tx, bufOrange2...)
+		case 6:
+			tx = append(tx, bufMag...)
 		default:
 			tx = append(tx, buf...)
 		}
